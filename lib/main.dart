5604281bb372eb75c64b23a878341cf972a25b52
Minh Le <minhb1900347@student.ctu.edu.vn>
2022-09-15 19:55:50 +0700

    Xay dung trang tong quan cac san pham

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/products_manager.dart';
+import 'ui/products/product_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,9 +25,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: SafeArea(
-        child: ProductDetailScreen(
-          ProductsManager().items[0],
-        ),
+        child: ProductsOverviewScreen(),
       ),
     );
   }
