c99389548417d0b68a415acc2cfc3b45004da166
Minh Le <minhb1900347@student.ctu.edu.vn>
2022-10-18 09:16:31 +0700

    Quan ly trang thai voi provider - ProductsManager

--- lib/ui/products/user_product_list_tile.dart
+++ lib/ui/products/user_product_list_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/ui/products/products_manager.dart';
 import '../../models/product.dart';
+import 'package:provider/provider.dart';
 
 class UserProductListTile extends StatelessWidget {
   final Product product;
@@ -28,15 +30,35 @@ class UserProductListTile extends StatelessWidget {
       );
     }
 
-    Widget buildDeleteButton(BuildContext context) {
-      return IconButton(
-        icon: const Icon(Icons.delete),
-        onPressed: () async {
-          print('Delete a product');
-        },
-        color: Theme.of(context).errorColor,
-      );
-    }
+    // Widget buildDeleteButton(BuildContext context) {
+    //   return IconButton(
+    //     icon: const Icon(Icons.delete),
+    //     onPressed: () async {
+    //       print('Delete a product');
+    //     },
+    //     color: Theme.of(context).errorColor,
+    //   );
+    // }
+
+  Widget buildDeleteButton(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.delete),
+      onPressed: () {
+        context.read<ProductsManager>().deleteProduct(product.id!);
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+            const SnackBar(
+              content: Text(
+                'Product deleted',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
+      },
+    color: Theme.of(context).errorColor,
+    );
+  }
 
     Widget buildEditButton(BuildContext context) {
       return IconButton(
